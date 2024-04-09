5de4190624ca4f16cfecc42025b44aecd9988daf
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-04-09 16:09:16 +0700

    setup environment

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="MainLayout" class="w-full fixed z-50">
+    <div id="TopMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
+      <ul
+        class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#FAFAFA] max-w-[1200px]"
+      >
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Sell on AliExpress
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Cookies Preferences
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Help
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Buyer Protection
+        </li>
+        <li class="px-3 hover:text-[#FF4646] cursor-pointer">
+          <Icon name="ic:sharp-install-mobile" size="17" />
+          App
+        </li>
+        <li
+          @mouseenter="isAccountMenu = true"
+          @mouseleave="isAccountMenu = true"
+          class="relative flex items-center px-2.5 hover:text-[#FF4646]"
+          :class="
+            isAccountMenu
+              ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
+              : 'border border-[#FAFAFA]'
+          "
+        >
+          <Icon name="ph:user-thin" size="17" />
+          Account
+          <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+          <div
+            id="AccountMenu"
+            v-if="isAccountMenu"
+            class="absolute bg-white w-[220px] h-[100px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
+          >
+            <div v-if="true">
+              <div
+                class="flex items-center justify-end flex-col text-semibold text-[15px] my04 px-3"
+              >
+                <div>Wellcome to AliExPress</div>
+                <NuxtLink
+                  to="/auth"
+                  class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
+                >
+                  Login/Register
+                </NuxtLink>
+              </div>
+            </div>
+            <div class="border-b"></div>
+            <ul class="bg-white">
+              <li
+                @click="navigateTo('/orders')"
+                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+              >
+                My Orders
+              </li>
+              <li
+                v-if="true"
+                @click="client.auth.signOut()"
+                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+              >
+                Sign out
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup>
+let isAccountMenu = ref(false);
+</script>
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <MainLayout>
+    <div id="IndexPage">IndexPage</div>
+  </MainLayout>
+</template>
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+</script>
