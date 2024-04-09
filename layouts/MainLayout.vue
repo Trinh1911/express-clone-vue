f04fd572d01d7ab556bdf9a299b0e665255bd324
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-04-09 18:08:36 +0700

    create header

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -79,9 +79,84 @@
         </li>
       </ul>
     </div>
+    <div id="MainHeader" class="flex items-center w-full bg-white">
+      <div
+        class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
+      >
+        <NuxtLink to="/" class="min-w-[170px]">
+          <img src="./../public/images/AliExpress-logo.png" width="170" />
+        </NuxtLink>
+        <div class="max-w-[700px] w-full md:block hidden">
+          <div class="relative">
+            <div
+              class="flex items-center border-2 border-[#FF4646] rounded-md w-full"
+            >
+              <input
+                class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                placeholder="kitchen accessories"
+                type="text"
+                v-model="searchItem"
+              />
+              <Icon
+                v-if="isSearching"
+                name="eos-icons:loading"
+                size="25"
+                class="mr-2"
+              />
+              <button
+                class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]"
+              >
+                <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
+              </button>
+            </div>
+            <div class="absolute bg-white max-w-[700px] h-auto w-full">
+              <div class="p-1">
+                <NuxtLink
+                  :to="`/item/1`"
+                  class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+                >
+                  <div class="flex items-center">
+                    <img
+                      class="rounded-md"
+                      width="40"
+                      src="https://i.pinimg.com/originals/37/2b/ea/372beacf683f0062fa749d2fbc94184f.jpg"
+                    />
+                    <div class="truncate ml-2">testing</div>
+                  </div>
+                  <div class="truncate">$9000</div>
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+        </div>
+        <NuxtLink to="/shoppingcart" class="flex items-center">
+          <button
+            class="relative md:block hidden"
+            @mouseenter="isCartHover = true"
+            @mouseleave="isCartHover = false"
+          >
+            <span
+              class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#FF4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+            >
+              0
+            </span>
+            <div class="min-w-[40px]">
+              <Icon
+                name="ph:shopping-cart-simple-light"
+                size="33"
+                :color="isCartHover ? '#FF4646' : ''"
+              />
+            </div>
+          </button>
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
 let isAccountMenu = ref(false);
+let isSearching = ref(false);
+let searchItem = ref("");
+let isCartHover = ref(false);
 </script>
 <style></style>
