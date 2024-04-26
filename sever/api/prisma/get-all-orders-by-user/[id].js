177271d140859cf19d0fae0e0331b31fa4417891
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-04-26 13:24:09 +0700

    crud api

--- sever/api/prisma/get-all-orders-by-user/[id].js
+++ sever/api/prisma/get-all-orders-by-user/[id].js
@@ -1,16 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export default defineEventHandler(async (event) => {
-    let orders = await prisma.orders.findMany({
-        where: { userId: event.context.params.userId},
-        orderBy: {id: "desc"},
+  let orders = await prisma.orders.findMany({
+    where: { userId: event.context.params.userId },
+    // sắp xếp kết quả theo thứ tự giảm dần
+    orderBy: { id: "desc" },
+    // truy vấn thông tin từ bảng liên kết là orderItem
+    include: {
+      orderItem: {
+        // truy vấn thông tin từ bảng liên kết product
         include: {
-            orderItem: {
-                include: {
-                    product: true
-                }
-            }
-        }
-
-    })
-})
+          // product: true nghĩa là lấy toàn bộ thông tin về sản phẩm b
+          product: true,
+        },
+      },
+    },
+  });
+  return orders;
+});
--- sever/api/prisma/search-by-name/[id].js
+++ sever/api/prisma/search-by-name/[id].js
@@ -1,11 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+// event là đối tượng chứa thông tin về yêu cầu đến bao gồm 
+// dữ liệu từ người dùng
 export default defineEventHandler(async (event)=> {
     let items = await prisma.products.findMany({
+        // giới hạn kết quả trả về là 5 mục đầu tiên
         take: 5,
         where: {
+            // kiểm tra trường title
             title: {
+                // lấy thông tin truy vấn 
                 contains: event.context.params.id,
+                // tìm kiếm không phân biệt chứ hoa chữ thường
                 mode: 'insensitive'
             }
         }
